15d43eff7b23c592b740c9dae62a32a498db4fdf
Sapenuk <asapenuk@am.sony.com>
2016-09-29 17:21:49 -0700

    added jdbc

---
+++ src/main/java/hello/Customer.java
@@ -0,0 +1,23 @@
+package hello;
+
+/**
+ * Created by asapenuk on 9/29/2016.
+ */
+public class Customer
+{
+    private long id;
+    private String firstName, lastName;
+
+    public Customer(long id, String firstName, String lastName)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
+    }
+}
